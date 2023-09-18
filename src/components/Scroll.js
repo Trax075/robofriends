80140d9ce56c57e3df4be4615b52aaf2cd37f8ad
Trax075 <trakic.amel@hotmail.com>
2023-09-18 18:15:36 +0200

    Adding my site

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './Scroll.css';
 
 
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '900px' }}>
+        <div className="scrollStyle">
             {props.children}
         </div>
 
